910219d9dc7af5bdc66a12b28631ffae6faac022
meethatkar <meethatkar000@gmail.com>
2025-06-25 22:58:56 +0530

    made horror page desktop reponsive

--- pages/horror/horror.js
+++ pages/horror/horror.js
@@ -8,9 +8,9 @@ let easyStories = [
     {
         title: "The Silent Ward",
 
-        summary: " a night guard working in an abandoned hospital. His routine check of a desolate isolation wing takes a terrifying turn when strange noises lead him to Room 7. Inside, he discovers unsettling markings on the walls and a single, chilling object, hinting at the unsettling past of the place and the lingering presence of its former inhabitants.",
+        summary: " a night guard working in an abandoned hospital. His routine check of a desolate isolation wing takes a terrifying turn when strange noises lead him to Room 7. Inside, he discovers unsettling markings on the walls and a single, chilling object, hinting at the unsettling past of the place and the lingering presence of its former inhabitants. night guard working in an abandoned hospital. His routine check of a desolate isolation wing takes a terrifying turn when strange noises lead him to Room 7. Inside, he discovers unsettling markings on the walls and a single, chilling object, hinting at the unsettling past of the place and the lingering presence of its former inhabitants.",
 
-        hints: ['wing', 'broken', 'spine', 'Room 7', 'bare', 'scratches', 'procelain eyes', 'wishpher'],
+        hints: ['wing', 'broken', 'spine', 'Room 7', 'bare', 'scratches', 'procelain eyes', 'wishpher','wing', 'broken', 'spine', 'Room 7', 'bare', 'scratches', 'procelain eyes', 'wishpher'],
 
         userSelection: [],
         stroy: "The chill in the old hospital's abandoned wing wasn't just from the wishper windows. Liam, the new night guard, felt a prickle down his room u7 as he checked the locked doors of the isolation ward. A faint, rhythmic scratching echoed from Scratches. He hesitated, flashlight beam trembling, before pushing the heavy door open The room was Porcelain eye, save for a single, rusted bed.On the dust- covered wall, hundreds of tiny, parallel broken marred the paint, forming an unsettling pattern.It wasn't just one set; they overlapped, intertwining. As his light swept across the floor, he saw a single, antique doll, its spine staring blankly at the wall. Then, from the darkness behind him, a bare, \"We're still here...",
@@ -23,7 +23,7 @@ function renderHints() {
     easyStories[randomStoryNumber].hints.forEach((hint) => {
         let hintDiv = document.createElement("span");
         hintDiv.innerText = hint;
-        hintDiv.classList.add("hint-btn");
+        window.innerWidth > 756 ? hintDiv.classList.add("hint-desktop") : hintDiv.classList.add("hint-btn")
         hintParent.appendChild(hintDiv);
     })
 }
@@ -33,7 +33,7 @@ function renderSummary(){
     let summaryParent = document.querySelector("#summary");
     let summaryDiv = document.createElement("p");
     summaryDiv.innerText = easyStories[randomStoryNumber].summary;   
-    summaryDiv.classList.add("summary-text"); 
+    window.innerWidth>765 ? summaryDiv.classList.add("summary-text-desktop"):summaryDiv.classList.add("summary-text"); 
     summaryParent.appendChild(summaryDiv);
 }
 
@@ -42,7 +42,7 @@ function renderTitle(){
     let titleParent = document.querySelector("#title");
     let titleDiv = document.createElement("h1");
     titleDiv.innerText = easyStories[randomStoryNumber].title;
-    titleDiv.classList.add("story-title");
+    window.innerWidth > 765 ? titleDiv.classList.add("story-title-desktop") : titltDiv.classList.add("story-title-mobile");
     titleParent.appendChild(titleDiv);
 }
 
@@ -57,7 +57,13 @@ function timer() {
         }, 1000*i)
     }
 }
-// timer();
+
+if(window.innerWidth>765){
+    let body = document.getElementsByTagName("body");
+    body[0].classList.add("body-desktop");
+}
+
+timer();
 renderTitle();
 renderSummary();
 renderHints();
